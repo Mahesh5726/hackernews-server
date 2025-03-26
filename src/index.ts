826fd991dcca0a3cd1bc6a744b6cd7e1f1e81ec2
Maheshr57 <mahem7080@gmail.com>
2025-03-26 14:16:56 +0530

    Add authentication functionality with sign-up and log-in routes, and JWT token generation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { serve } from '@hono/node-server'
-import { allRoutes } from './routes/routes-index'
+import "dotenv/config";
+import { serve } from "@hono/node-server";
+import { allRoutes } from "./routes/routes-index";
 
 serve(allRoutes, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+  console.log(`Server is running on http://localhost:${info.port}`);
+});
--- src/routes/routes-index.ts
+++ src/routes/routes-index.ts
@@ -1,11 +1,14 @@
 import { Hono } from "hono";
+import { authenticationRoutes } from "./authentication-routes";
 
-export const allRoutes = new Hono()
+export const allRoutes = new Hono();
+
+allRoutes.route("/auth", authenticationRoutes);
 
 allRoutes.get("/ping", (c) => {
-    return c.json({
-        message: "pong"
-    })
-})
+  return c.json({
+    message: "pong",
+  });
+});
 
 
