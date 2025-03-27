5f9f115d3396a698936d66139f5a8b7d50a493d4
Maheshr57 <mahem7080@gmail.com>
2025-03-27 14:19:38 +0530

    Add post types and error handling enums for fetching, creating, and deleting posts

---
+++ src/controllers/posts/posts-types.ts
@@ -0,0 +1,35 @@
+import type { Post } from "@prisma/client"
+
+
+export type GetPostsResult = {
+    posts: Post[]
+}
+
+export enum GetPostsError {
+  POSTS_NOT_FOUND = "POSTS_NOT_FOUND",
+  PAGE_BEYOND_LIMIT = "PAGE_BEYOND_LIMIT",
+  UNKNOWN = "UNKNOWN"
+}
+
+export type CreatePostResult = {
+    post: Post
+}
+
+export enum CreatePostError {
+    USER_NOT_FOUND = "USER_NOT_FOUND",
+    TITLE_REQUIRED = "TITLE_REQUIRED",
+    UNKNOWN = "UNKNOWN"
+}
+
+export type DeletePostResult = {
+    post: Post
+}
+
+export enum DeletePostError {
+    USER_NOT_FOUND = "USER_NOT_FOUND",
+    POST_NOT_FOUND = "POST_NOT_FOUND",
+    UNAUTHORIZED = "UNAUTHORIZED",
+    UNKNOWN = "UNKNOWN"
+}
+
+
