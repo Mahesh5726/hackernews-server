bc33ca8c86daa0878d7500d9b97d101b01ca9155
Mahesh5726 <maheshr070204@gmail.com>
2025-04-23 19:53:53 +0530

    Add authentication middleware and CORS support to routes

--- src/routes/routes-index.ts
+++ src/routes/routes-index.ts
@@ -6,11 +6,23 @@ import { usersRoutes } from "./users-routes";
 import { postsRoutes } from "./posts-routes";
 import { likesRoutes } from "./likes-routes";
 import { commentsRoutes } from "./comments-routes";
+import { authRoute } from "./middleware/session-middleware";
+import { cors } from "hono/cors";
 
 export const allRoutes = new Hono();
 
+allRoutes.use(
+  cors({
+    origin: "http://localhost:4000",
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
 allRoutes.get("/ui", swaggerUI({ url: "/docs" }));
 allRoutes.route("/", swaggerDocument);
+allRoutes.route("/api/auth", authRoute);
 allRoutes.route("/auth", authenticationRoutes);
 allRoutes.route("/users", usersRoutes);
 allRoutes.route("/posts", postsRoutes);
