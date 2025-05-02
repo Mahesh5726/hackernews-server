e96cde5f9090bd9b2411fbedadde2a6809987cf9
Mahesh5726 <maheshr070204@gmail.com>
2025-05-02 10:25:35 +0530

    Update CORS configuration to allow requests from production URL and comment out localhost origin for deployment readiness

--- src/routes/routes-index.ts
+++ src/routes/routes-index.ts
@@ -11,7 +11,8 @@ export const allRoutes = new Hono();
 
 allRoutes.use(
   cors({
-    origin: "http://localhost:4000",
+    // origin: "http://localhost:4000",
+    origin: ["https://hackernews-ui-lemonisland.vercel.app"],
     allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
     allowHeaders: ["Content-Type", "Authorization", "token"],
