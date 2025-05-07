f53f96e64f1d794f00d63d14e303453c69d0d644
Mahesh5726 <maheshr070204@gmail.com>
2025-05-07 15:14:46 +0530

    Refactor authentication routes to utilize new session middleware and remove legacy controllers
    - Deleted authentication controllers and types to streamline the codebase.
    - Updated authentication routes to use the new session middleware for handling requests.
    - Simplified the structure of authentication routes for improved clarity and maintainability.

--- src/routes/authentication/authentication-routes.ts
+++ src/routes/authentication/authentication-routes.ts
@@ -1,63 +1,8 @@
-import { Hono } from "hono";
-import { signIn, signUp } from "./authentication-controllers";
-import {
-  LogInWithUsernameAndPasswordError,
-  SignUpWithUsernameAndPasswordError,
-} from "./authentication-types";
+import auth from "../../lib/auth";
+import { createUnsecureRoute } from "../middleware/session-middleware";
 
-export const authenticationRoutes = new Hono();
+export const authenticationsRoutes = createUnsecureRoute();
 
-authenticationRoutes.post("/sign-up", async (c) => {
-  const { username, password, name, email } = await c.req.json();
-  try {
-    const result = await signUp({
-      username,
-      password,
-      name,
-      email,
-    });
-
-    c.header(
-      "Set-Cookie",
-      `__Secure-better-auth.session_token=${result.token}; Path=/; Max-Age=604800; HttpOnly; SameSite=None; Secure=true`
-    );
-
-    return c.json({ data: result }, 200);
-  } catch (error) {
-    if (error === SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME) {
-      return c.json({ error: "Username already exists" }, 409);
-    }
-    return c.json({ error: "Unknown error" }, 500);
-  }
-});
-
-authenticationRoutes.post("/log-in", async (c) => {
-  try {
-    const { username, password } = await c.req.json();
-
-    const result = await signIn({
-      username,
-      password,
-    });
-
-    c.header(
-      "Set-Cookie",
-      `__Secure-better-auth.session_token=${result?.token}; Path=/; Max-Age=604800; HttpOnly; SameSite=None; Secure=true`
-    );
-
-    return c.json(
-      {
-        data: result,
-      },
-      200
-    );
-  } catch (error) {
-    if (
-      error === LogInWithUsernameAndPasswordError.INCORRECT_USERNAME_OR_PASSWORD
-    ) {
-      return c.json({ error: "Incorrect username or password" }, 401);
-    }
-
-    return c.json({ error: "Unknown error" }, 500);
-  }
+authenticationsRoutes.use((c) => {
+  return auth.handler(c.req.raw);
 });
